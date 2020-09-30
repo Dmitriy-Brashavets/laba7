f2e1a4afd48854b886d46dc8593d273937a27bfc
Dmitriy <72145339+Dmitriy-Brashavets@users.noreply.github.com>
2020-10-01 00:51:12 +0300

    Update laba7.cpp

--- laba7.cpp
+++ laba7.cpp
@@ -1,54 +1,57 @@
-#include <QVector>
+#include <vector>
 #include <math.h>
 #include <iostream>
 
 using std::cout;
 using std::endl;
+using std::swap;
+using std::vector;
 
 void Task1() {
-    cout << "task1: " << endl;
-    QVector <int> vec, rez;
-    vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13 };
-    for (int i = 0; i < vec.size(); i += 2)
-        rez += vec[i];
-    for (int i = 1; i < vec.size(); i += 2)
-        if(i >= vec.size()) break;
-        else rez += vec[i];
-    cout << "initial: ";
-    foreach(int el, vec)
-        cout << el << " ";
-    cout << endl << "rezult: ";
-    foreach(int el, rez)
-        cout << el << " ";
-    cout << endl << "========================" << endl;
+	cout << "task1: " << endl;
+	vector <int> vec, rez;
+	vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13 };
+	for (int i = 0; i < vec.size(); i += 2)
+		rez.push_back(vec[i]);
+	for (int i = 1; i < vec.size(); i += 2)
+		rez.push_back(vec[i]);
+	cout << "initial: ";
+	for (int el : vec)
+		cout << el << " ";
+	cout << endl << "rezult: ";
+	for (int el : rez)
+		cout << el << " ";
+	cout << endl << "========================" << endl;
 }
 
 void Task2() {
-    cout << "task2: " << endl;
-    QVector <int> vec;
-    int rez = 1;
-    vec = { 1 , 5, 2, 3, -6, 5, 5, 2 };
-    QVector <int> :: iterator iter1, iterMax, iterMin;
-    int max = 0, min = 100;
-    for(iter1 = vec.begin(); iter1 != vec.end(); ++iter1) {
-        if (max < abs(*iter1)) {
-            max = abs(*iter1);
-            iterMax = iter1;
-        }
-        if (min > abs(*iter1)) {
-            min = abs(*iter1);
-            iterMin = iter1;
-        }
-    }
-    for(iter1 = iterMin; iter1 != iterMax + 1; ++iter1)
-        rez *= (*iter1);
-    cout << "initial: ";
-    foreach (int el, vec)
-        cout << el << " ";
-    cout <<"\nrez: " << rez;
+	cout << "task2: " << endl;
+	vector <int> vec;
+	int rez = 1;
+	vec = { 1 , 5, 2, 3, -6, 5, 5, 2 };
+	int iMax, iMin;
+	int max = -1, min = 100;
+	for (int i = 0; i < vec.size(); i++) {
+		if (max < abs(vec[i])) {
+			max = abs(vec[i]);
+			iMax = i;
+		}
+		if (min > abs(vec[i])) {
+			min = abs(vec[i]);
+			iMin = i;
+		}
+	}
+	if (iMin > iMax)
+		swap(iMin, iMax);
+	for (int i = iMin + 1; i < iMax; i++)
+		rez *= vec[i];
+	cout << "initial: ";
+	for (int el : vec)
+		cout << el << " ";
+	cout << "\nrez: " << rez;
 }
 
 int main() {
-    Task1();
-    Task2();
+	Task1();
+	Task2();
 }
